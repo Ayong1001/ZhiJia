5628252849759e22180ee291d522d91f6d458366
Ayong <911612013@qq.com>
2023-06-09 15:58:10 +0800

    fix: 环境切换、默认图片修改

--- common/operate.js
+++ common/operate.js
@@ -3,6 +3,6 @@ export default {
   prod: 'http://nas.zmyus.top:9876',
   dev: 'http://172.16.1.32:9000',
   // url: 'http://172.16.1.32:9876',
-  url: 'http://nas.zmyus.top:9876'
+  url: 'http://www.zhikao168.com:9876'
 }
 	
